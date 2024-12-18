6bbb62b3dde101d4750a367f0103a7736c091c0f
gominsu08 <minsu080618@gmail.com>
2024-12-18 23:25:01 +0900

    fix : SwordData

--- Assets/Gwamegi/01_Scripts/Sword/Sword.cs
+++ Assets/Gwamegi/01_Scripts/Sword/Sword.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.FilePathAttribute;
 
 public abstract class Sword : MonoBehaviour
 {
+    [SerializeField] private GameObject _visual;
     protected SwordDataSO m_SwordDataSO;  
     protected LayerMask m_LayerMask;
     protected Vector2 m_Position;
@@ -16,8 +18,29 @@ public abstract class Sword : MonoBehaviour
         m_RbCompo = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ThrowSword(new Vector2(6, 3));
+        }
 
-    public abstract void ThrowSword(Vector2 targetPos);
+        if ((m_Position.x + 0.5f > transform.position.x && m_Position.x - 0.5f < transform.position.x) && (m_Position.y + 0.5f > transform.position.y && m_Position.y - 0.5f < transform.position.y))
+        {
+            m_RbCompo.velocity = Vector2.zero;
+        }
+    }
+
+
+    public virtual void ThrowSword(Vector2 targetPos)
+    {
+        DistanceCalculation(targetPos);
+
+        Vector2 direction = (targetPos - (Vector2)transform.position);
+        float desiredAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _visual.transform.rotation = Quaternion.AngleAxis(desiredAngle - 45, Vector3.forward);
+        m_RbCompo.velocity = direction.normalized * m_SwordDataSO.speed;
+    }
 
     public virtual void DistanceCalculation(Vector2 targetPos)
     {
